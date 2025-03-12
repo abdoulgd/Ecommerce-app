4511a5efe22a6b1b84b0f4dbf9a55ae7b7be6dca
Abdoul Goudoussy Diallo <dialloabdoulgoudoussy97@gmail.com>
2025-03-12 16:08:57 +0100

    add backand spring boot Entities, Repositories, Rest API & connexion to my DataBase MySQL

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.luv2code.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+// @Data  -- known bug with Lombok
+@Getter
+@Setter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String category;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
