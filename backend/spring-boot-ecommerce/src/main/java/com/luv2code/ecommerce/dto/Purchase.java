a18d4d7ccb5e21b9c0180babde9c89f729122b4c
Abdoul Goudoussy Diallo <dialloabdoulgoudoussy97@gmail.com>
2025-09-05 18:24:30 +0200

    feat(section23): Checkout Form - Save the order to Database Backend

---
+++ backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dto/Purchase.java
@@ -0,0 +1,23 @@
+package com.luv2code.ecommerce.dto;
+
+import com.luv2code.ecommerce.entity.Address;
+import com.luv2code.ecommerce.entity.Customer;
+import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class Purchase {
+
+    private Customer customer;
+
+    private Order order;
+
+    private Address shippingAddress;
+
+    private Address billingAddress;
+
+    private Set<OrderItem> orderItems;
+}
